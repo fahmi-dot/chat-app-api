61ccdd0595f947c28c8a8abc4a48adfdc0975cc7
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-28 05:27:58 +0700

    initial commit

---
+++ src/main/java/com/fahmi/chatappapi/ChatAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.fahmi.chatappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatAppApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fahmi/chatappapi/ChatAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahmi.chatappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
