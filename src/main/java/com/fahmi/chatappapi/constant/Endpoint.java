924913bad719e56b6c4ad9aa63146f7b00f02deb
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-28 08:32:23 +0700

    chore: add Endpoint constant

---
+++ src/main/java/com/fahmi/chatappapi/constant/Endpoint.java
@@ -0,0 +1,7 @@
+package com.fahmi.chatappapi.constant;
+
+public class Endpoint {
+    public static final String API = "/api/v1";
+    public static final String AUTH = API + "/auth";
+    public static final String USER = API + "/user";
+}
