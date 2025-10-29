39e9d2eb0c9fa6c13daae048480805adcf27bcfe
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-29 08:09:42 +0700

    feat: add new Derived Query Methods

--- src/main/java/com/fahmi/chatappapi/repository/RoomRepository.java
+++ src/main/java/com/fahmi/chatappapi/repository/RoomRepository.java
@@ -4,6 +4,9 @@ import com.fahmi.chatappapi.entity.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoomRepository extends JpaRepository<Room, String> {
+    Optional<Room> findByRoomKey(String roomKey);
 }
--- src/main/java/com/fahmi/chatappapi/repository/UserRepository.java
+++ src/main/java/com/fahmi/chatappapi/repository/UserRepository.java
@@ -4,6 +4,15 @@ import com.fahmi.chatappapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByPhoneNumber(String phoneNumber);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
