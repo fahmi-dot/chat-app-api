9478115143ba9ec5fc2e3a22976ce75529d89558
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-10-29 08:04:39 +0700

    feat: update SecurityConfig to use JwtAuthFilter and add new Endpoint

--- src/main/java/com/fahmi/chatappapi/config/SecurityConfig.java
+++ src/main/java/com/fahmi/chatappapi/config/SecurityConfig.java
@@ -1,18 +1,23 @@
 package com.fahmi.chatappapi.config;
 
+import com.fahmi.chatappapi.constant.Endpoint;
+import com.fahmi.chatappapi.util.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorizeRequest -> {
@@ -22,10 +27,17 @@ public class SecurityConfig {
                                     "/v3/api-docs/**",
                                     "/swagger-resources/**",
                                     "/swagger-ui.html",
-                                    "/actuator/**"
+                                    "/actuator/**",
+                                    Endpoint.AUTH + "/**"
                             ).permitAll()
                             .anyRequest().authenticated();
                 })
+                .addFilterAfter(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/fahmi/chatappapi/constant/Endpoint.java
+++ src/main/java/com/fahmi/chatappapi/constant/Endpoint.java
@@ -4,4 +4,5 @@ public class Endpoint {
     public static final String API = "/api/v1";
     public static final String AUTH = API + "/auth";
     public static final String USER = API + "/user";
+    public static final String CHAT = API + "/chat";
 }
