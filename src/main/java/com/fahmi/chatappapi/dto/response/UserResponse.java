810bb61cae4b4ae3becb83e25990ca2ff8cdb732
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-02 10:32:47 +0700

    feat: update UserResponse

--- src/main/java/com/fahmi/chatappapi/dto/response/UserResponse.java
+++ src/main/java/com/fahmi/chatappapi/dto/response/UserResponse.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class UserResponse {
+    private String id;
     private String username;
     private String displayName;
     private String phoneNumber;
--- src/main/java/com/fahmi/chatappapi/mapper/UserMapper.java
+++ src/main/java/com/fahmi/chatappapi/mapper/UserMapper.java
@@ -19,6 +19,7 @@ public class UserMapper {
 
     public static UserResponse toResponse(User user) {
         return UserResponse.builder()
+                .id(user.getId())
                 .username(user.getUsername())
                 .displayName(user.getDisplayName())
                 .phoneNumber(user.getPhoneNumber())
