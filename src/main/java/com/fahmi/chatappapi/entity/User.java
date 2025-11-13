554b6c8ef692e008f52280da28ed6ddf810e85cb
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-13 20:41:53 +0700

    feat: add email column to User entity

--- src/main/java/com/fahmi/chatappapi/entity/User.java
+++ src/main/java/com/fahmi/chatappapi/entity/User.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
 
@@ -32,14 +33,28 @@ public class User implements UserDetails {
     @Column(name = "phone_number", unique = true, nullable = false)
     private String phoneNumber;
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
     @Column(name = "avatar_url", nullable = false)
     private String avatarUrl;
 
     private String bio;
 
+    @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
+
+    @Column(name = "is_verified", nullable = false)
+    private boolean isVerified;
+
+    @Column(name = "verification_code")
+    private String verificationCode;
+
+    @Column(name = "code_expires_at")
+    private LocalDateTime codeExpiresAt;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
