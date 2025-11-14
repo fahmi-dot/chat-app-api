6e9db29e8ba9bc7b02e5d7be6ead8a6268388309
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-14 14:03:50 +0700

    refactor: change from SimpleMailMessage to MimeMessage

--- src/main/java/com/fahmi/chatappapi/service/impl/EmailServiceImpl.java
+++ src/main/java/com/fahmi/chatappapi/service/impl/EmailServiceImpl.java
@@ -1,22 +1,43 @@
 package com.fahmi.chatappapi.service.impl;
 
 import com.fahmi.chatappapi.service.EmailService;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
     private final JavaMailSender mailSender;
 
     public void sendVerificationCode(String receiverEmail, String verificationCode) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(receiverEmail);
-        message.setSubject("Verification Code");
-        message.setText("Your Hello! verification code: " + verificationCode);
-        mailSender.send(message);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, false, "UTF-8");
+
+            helper.setTo(receiverEmail);
+            helper.setSubject("Hello! account verification code");
+            helper.setFrom(senderEmail, "Hello!");
+
+            String body = """
+                <h1>Hello!</h1>
+                <p>Here is your <b>Hello!</b> account verification code: <b>%s<b/></p><br/>
+                <p>Thanks!</p>
+                <p>The Hello! team</p>
+                """.formatted(verificationCode);
+
+            helper.setText(body, true);
+
+            mailSender.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
