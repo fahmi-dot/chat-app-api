63e6f9696f63d886442073d20405d9e1a5a2a431
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-14 16:19:46 +0700

    chore: add exception handler files

---
+++ src/main/java/com/fahmi/chatappapi/exception/CustomException.java
@@ -0,0 +1,33 @@
+package com.fahmi.chatappapi.exception;
+
+public class CustomException {
+    public static class BadRequestException extends RuntimeException {
+        public BadRequestException(String message) {
+            super(message);
+        }
+    }
+
+    public static class AuthenticationException extends RuntimeException {
+        public AuthenticationException(String message) {
+            super(message);
+        }
+    }
+
+    public static class AuthorizationException extends RuntimeException {
+        public AuthorizationException(String message) {
+            super(message);
+        }
+    }
+
+    public static class ResourceNotFoundException extends RuntimeException {
+        public ResourceNotFoundException(String message) {
+            super(message);
+        }
+    }
+
+    public static class ConflictException extends RuntimeException {
+        public ConflictException(String message) {
+            super(message);
+        }
+    }
+}
