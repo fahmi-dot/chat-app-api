8010aef8db9189f723073b052d52936638a6db6f
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-18 13:26:10 +0700

    feat: add UserUpdateRequest for update profile DTO's

---
+++ src/main/java/com/fahmi/chatappapi/dto/request/UserUpdateRequest.java
@@ -0,0 +1,10 @@
+package com.fahmi.chatappapi.dto.request;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateRequest {
+    private String username;
+    private String displayName;
+    private String password;
+}
