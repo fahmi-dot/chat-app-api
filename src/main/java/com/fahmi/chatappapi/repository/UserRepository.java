0c9bb6266b258986e7c3381025ad75a29cfcf357
Pahmiii <agilfahmirakasaputra073@gmail.com>
2025-11-23 19:55:23 +0700

    feat: add new variable in UserRepository for search user

--- src/main/java/com/fahmi/chatappapi/repository/UserRepository.java
+++ src/main/java/com/fahmi/chatappapi/repository/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -22,4 +23,5 @@ public interface UserRepository extends JpaRepository<User, String> {
     @Query(value = "SELECT nextval('username_seq')", nativeQuery = true)
     long getUsernameNumber();
 
+    List<User> findByUsernameIsContainingIgnoreCase(String username);
 }
